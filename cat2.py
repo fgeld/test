7f0f2bd1d9b2a3f1b661ea2e3fcb2b7847b4a3df
Frank Geldner <frgeldne@cisco.com>
2021-01-05 13:03:57 +0100

    2nd commit

---
+++ cat2.py
@@ -0,0 +1,8 @@
+
+@single_request_timeout.setter
+def single_request_timeout(self, value):
+    """The timeout (seconds) for a single HTTP REST API request."""
+    check_type(value, int, optional=True)
+    if value is not None and value <= 0:
+        raise ValueError("single_request_timeout must be positive interger")
+    self._single_request_timeout = value
